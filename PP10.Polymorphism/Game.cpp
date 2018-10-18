738433797192b0e221fd13c3523df02845e09b4f
jaeyong park <20161634@game.hoseo.edu>
2018-10-18 17:05:35 +0900

    10월 18일 PP10.Polymorphism

--- PP10.Polymorphism/Game.cpp
+++ PP10.Polymorphism/Game.cpp
@@ -23,9 +23,17 @@ bool Game::init(const char* title, int xpos, int ypos,
 			return false;
 		}
 		SDL_SetRenderDrawColor(m_pRenderer, 255, 255, 255, 255);
-		m_go.load(100, 100, 128, 82, "animate");
-		m_player.load(300, 300, 128, 82, "animate");
-		
+		m_go = new GameObject();
+		m_player = new Player();
+		m_enemy = new Enemy();
+
+		m_go->load(100, 100, 128, 82, "animate");
+		m_player->load(300, 300, 128, 82, "animate");
+		m_enemy->load(0, 0, 128, 82, "animate");
+
+		m_gameObject.push_back(m_go);
+		m_gameObject.push_back(m_player);
+		m_gameObject.push_back(m_enemy);
 	}
 	else
 	{
@@ -37,15 +45,19 @@ bool Game::init(const char* title, int xpos, int ypos,
 void Game::render()
 {
 	SDL_RenderClear(m_pRenderer);
-	m_go.draw(m_pRenderer);
-	m_player.draw(m_pRenderer);
+	for (std::vector<GameObject*>::size_type i = 0; i != m_gameObject.size(); i++)
+	{
+		m_gameObject[i]->draw(m_pRenderer);
+	}
 	SDL_RenderPresent(m_pRenderer);
 }
 
 void Game::update()
 {
-	m_go.update();
-	m_player.update();
+	for (std::vector<GameObject*>::size_type i = 0; i != m_gameObject.size(); i++)
+	{
+		m_gameObject[i]->update();
+	}
 }
 
 void Game::clean()
